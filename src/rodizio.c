c01ff137ac6fdb4acd11c80d5496680efe8645e2
Edilson Silva <edilson_js@yahoo.com.br>
2025-07-30 12:29:09 +0000

    Trabalhando com a estrutura if .. else if ..else

---
+++ src/rodizio.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+    int placa;
+    printf("Digite o número final da placa do seu veículo e tecle Enter\n");
+    scanf("%d",&placa);
+
+    if(placa == 1){
+        printf("Rodizio de Segunda-Feira\n");
+    }
+    else if(placa == 2){
+        printf("Rodizio de Segunda-Feira\n");
+    }
+    else if(placa == 3){
+        printf("Rodizio de Terça-Feira\n");
+    }
+    else if(placa == 4){
+        printf("Rodizio de Terça-Feira\n");
+    }
+    else if(placa == 5){
+        printf("Rodizio de Quarta-Feira\n");
+    }
+    else if(placa == 6){
+        printf("Rodizio de Quarta-Feira\n");
+    }
+    else if(placa == 7){
+        printf("Rodizio de Quinta-Feira\n");
+    }
+    else if(placa == 8){
+        printf("Rodizio de Quinta-Feira\n");
+    }   
+    else if(placa == 9){
+        printf("Rodizio de Sexta-Feia\n");
+    }
+    else if(placa == 0){
+        printf("Rodizio de Sexta-Feira\n");
+    }
+    else{
+        printf("Final de placa inválido!\n");
+    }
+    return 0;
+
+}
