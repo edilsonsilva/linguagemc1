2954f0969a15a608385d8eb6d6e828fbcdf69595
Edilson Silva <edilson_js@yahoo.com.br>
2025-08-05 12:01:29 +0000

    Exemplos de comandos while

---
+++ src/whiletabuada.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    system("clear");
+    int num, cont, rs;
+    cont = 1;
+    printf("Digite o número da tabuada que deseja fazer \n");
+    scanf("%d",&num);
+    while(cont <= 10){
+        rs = num * cont;
+        printf("%d x %d = %d\n",num,cont,rs);
+        cont++;
+    }
+    return 0;
+}
